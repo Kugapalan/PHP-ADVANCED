e7d1a2ce4218375ea57a3804221f4b360de2f1da
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-03 16:04:59 +0100

    Opdracht afgerond

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,10 @@
+<?php
+$persoon ["voornaam"] = "Kugapalan";
+$persoon ["achternaam"] = "Ganeshwaran";
+$persoon ["leeftijd"] = "18";
+$persoon ["opleiding"] = "Software Developer";
+
+echo $persoon["voornaam"] . " " . $persoon["achternaam"] . " is " . $persoon["leeftijd"] . " jaar oud" . " en is student aan het ROC van Amsterdam aan de opleiding " . $persoon ["opleiding"];
+
+?>
+
