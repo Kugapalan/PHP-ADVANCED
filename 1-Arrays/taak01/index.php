c948cc6e66aac5827a24d26483f56954d3c26def
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-03 15:46:12 +0100

    Opdracht afgerond

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,10 @@
+<?php
+$studenten=array(
+    "voornaam"=>"Kugapalan",
+    "achternaam"=>"Ganeshwaran",
+    "leeftijd"=>"18",
+    "klas"=>"1A"
+);
+
+echo "Deze student heet ". $studenten["voornaam"] .$studenten["achternaam"] ." en is ". $studenten ["leeftijd"]. " jaar oud" . " en zit in klas " .$studenten ["klas"];
+  ?>
