d82bd2de2d8279beb6f5dfd3502edc9ef0b720f8
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-04 13:33:13 +0100

    Opdracht afgerond

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,14 @@
+<?php
+
+$leeftijd=0;
+while($leeftijd<18){
+
+    echo "<li> Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li>";
+    $leeftijd++;
+}
+
+echo "Ik ben 18 jaar oud dus ik heb stemrecht!"
+
+
+
+?>
