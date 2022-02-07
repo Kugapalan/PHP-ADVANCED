94d573d5c673ebf29f36d6d08c9d3bfe66694c02
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-07 09:41:46 +0100

    Opdracht afgerond

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -1,12 +1,13 @@
 <?php
-
 $leeftijd=0;
+
 while($leeftijd<18){
 
     echo "<ul> <li> Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li> </ul>";
+    
     $leeftijd++;
 }
 
-echo "Ik ben 18 jaar oud dus ik heb stemrecht!"
+echo "<ul> <li>Ik ben 18 jaar oud dus ik heb stemrecht!</li> </ul>"
 
 ?>
--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -3,6 +3,6 @@
 for($leeftijd=0;$leeftijd<18;$leeftijd++){
 echo "<ul><li> Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li></ul>";}
 
-echo "Ik ben 18 jaar oud dus ik heb stemrecht!"
+echo "<ul> <li>Ik ben 18 jaar oud dus ik heb stemrecht!</li> </ul>"
 
 ?>
