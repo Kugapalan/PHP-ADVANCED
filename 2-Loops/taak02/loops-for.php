21078024e6d80bbb8f6099bf452292bb408d7c25
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-08 13:25:59 +0100

    Opdracht afgerond

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -1,8 +1,10 @@
+<ul>
 <?php
 
 for($leeftijd=0;$leeftijd<18;$leeftijd++){
-echo "<ul><li> Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li></ul>";}
+echo "<li> Ik ben $leeftijd jaar, dus ik mag nog niet stemmen </li>";}
 
-echo "<ul> <li>Ik ben 18 jaar oud dus ik heb stemrecht!</li> </ul>"
+echo " <li>Ik ben 18 jaar oud dus ik heb stemrecht!</li> "
 
 ?>
+</ul>
