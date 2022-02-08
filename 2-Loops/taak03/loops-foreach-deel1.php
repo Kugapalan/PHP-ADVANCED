a2a065d0f83542c5d8ccdcd71ccd2d807e3b7629
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-08 13:19:03 +0100

    Opdracht afgerond

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,11 @@
+<ul> 
+<?php
+$dagen = 
+array("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag");
+
+foreach ($dagen as $value){
+ echo "<li>$value</li> <br>";
+}
+
+?>
+</ul>
