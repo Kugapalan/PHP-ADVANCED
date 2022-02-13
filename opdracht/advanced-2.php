22e8e9d6ba0f00e73783372ad5e41666527b1d5d
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-13 17:17:50 +0100

    Opdracht afgerond

---
+++ opdracht/advanced-2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<style> 
+    table, tr, th{
+        border: <?php echo $_POST["tabelrand"]?>px black;
+        padding: <?php echo $_POST["celpadding"]?>px;
+        background-color: <?php echo $_POST["achtergrondkleur"]?>;
+        color:  <?php echo $_POST["tekstkleur"]?>;
+    }
+    </style>
+</head>
+
+<table>
+<tr>
+    <th>Key</th>
+    <th>Value</th>
+</tr>
+
+<?php 
+    $student = array("Naam"=>"Kugapalan",
+                     "Klas"=>"LCTSDO1A", 
+                     "Leeftijd"=>"18", 
+                     "Woonplaats"=>"Amsterdam", 
+                     "Muzieksmaak"=>"Hiphop", 
+                     "Sport"=>"Karate");
+
+    function maakTabel($sleutel, $waarde){ 
+        echo "<tr>";
+        echo "<td>$sleutel</td>";
+        echo "<td>$waarde</td>";
+        echo "</tr>";
+     } 
+
+ foreach($student as $key => $value){
+     maakTabel($key, $value);
+}
+?>
+</table>
+</form>
+</body>
+</html>
