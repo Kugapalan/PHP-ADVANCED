c1fe52df684e94ef7d83d3909d3c666bf9f6ba52
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-02-20 18:56:02 +0100

    Opdracht afgerond

--- 3-Forms/taak03/mijnmuziek.php
+++ 3-Forms/taak03/mijnmuziek.php
@@ -5,22 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mijnmuziek</title>
-    <form action="invoer.html" method="GET">
 </head>
 <body>
-    
-</body>
-</html>
 
 <?php
 echo "Artiest is " . $_GET ["artiest"];
-echo "<br>";
+echo "<p>";
 echo "Titel is " .$_GET ["titel"];
-echo "<br>";
+echo "<p>";
 echo "Het jaar van uitkomst is " . $_GET ["Jaarvanuitkomst"];
-echo "<br>";
+echo "<p>";
 echo "Deze track is van het muzieksoort " . $_GET ["Muzieksoort"];
 ?>
+
+</body>
+</html>
 
 <! ---- http://localhost/Blok%204/PHP-ADVANCED/3-Forms/taak03/mijnmuziek.php?artiest=Wiz+Khalifa&titel=See+you+again&Jaarvanuitkomst=2015&Muzieksoort=Hiphop%2Frap&submit=verstuur
 ----->
